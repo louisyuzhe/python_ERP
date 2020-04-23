c3c07ce2ef303d7de90d2f26570d217c61c73e77
louisyuzhe <louisyuzhe@gmail.com>
2020-04-23 01:03:10 -0500

    Added new windows for sales

--- login.py
+++ login.py
@@ -5,22 +5,145 @@ import pandas as pd
 
 class saleWindow (QtWidgets.QWidget):
 
-    switch_window = QtCore.pyqtSignal(str)
-
-    def __init__(self):
+    switch_viewUpdateCus = QtCore.pyqtSignal(psycopg2.extensions.connection)
+    switch_cr8Order = QtCore.pyqtSignal(psycopg2.extensions.connection)
+    switch_salesReport = QtCore.pyqtSignal()  
+    
+    def __init__(self, connection):
         QtWidgets.QWidget.__init__(self)
         self.setWindowTitle('Sales Page')
         vBox = QtWidgets.QVBoxLayout()
-        self.button = QtWidgets.QPushButton('Query One')
-        self.button.clicked.connect(self.switch)
-        vBox.addWidget(self.button)
-
+        self.button1 = QtWidgets.QPushButton('View and update customer')
+        self.button1.clicked.connect(lambda: self.switch1(connection))
+        vBox.addWidget(self.button1)
+        
+        self.button2 = QtWidgets.QPushButton('Create an Order')
+        self.button2.clicked.connect(lambda: self.switch2(connection))
+        vBox.addWidget(self.button2)
+        
+        self.button3 = QtWidgets.QPushButton('Access sales reports')
+        self.button3.clicked.connect(self.switch3)
+        vBox.addWidget(self.button3)
+        
         self.setLayout(vBox)
+            
+    def switch1(self, connection):
+        self.switch_viewUpdateCus.emit(connection)      
+    def switch2(self, connection):
+        self.switch_cr8Order.emit(connection)
+    def switch3(self):
+        self.switch_salesReport.emit()      
+        
+class windowviewUpdateCus(QtWidgets.QWidget):
+    def __init__(self, connection):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Customer')
+        layout = QtWidgets.QGridLayout()
+        self.tableView = QtWidgets.QTableView(self)
 
-    def switch(self):
-        self.switch_window.emit("TEST")
+        self.button = QtWidgets.QPushButton('Display')
+        self.button.clicked.connect(lambda: self.btn_clk(connection))
 
+        layout.addWidget(self.button)
+        layout.addWidget(self.tableView)
+        self.setLayout(layout)
+    
+    def btn_clk(self, connection):
+        df=pd.DataFrame()
+        try:  
+            
+            query2 = "select * from customer;"
+            
+            df = pd.read_sql(query2, connection)                                        
+            
+            print("Table retrieved successfully in PostgreSQL ")
+            #print(df)
+        except (Exception, psycopg2.Error) as error :
+            print ("Error while connecting to PostgreSQL", error)
+        finally:
+            #closing database connection.
+                if(connection):      
+                    pass
+                    #connection.close()
+                    #print("PostgreSQL connection is closed")
+        #print(df)
+        model = DataFrameModel(df)
+        #print(model)
+        self.tableView.setModel(model)
 
+class windowcr8Order(QtWidgets.QWidget):
+    def __init__(self, connection):
+        QtWidgets.QWidget.__init__(self)
+        self.setWindowTitle('Orders')
+
+        vBox = QtWidgets.QVBoxLayout()
+        hBox1 = QtWidgets.QHBoxLayout()
+        hBox2 = QtWidgets.QHBoxLayout()
+        hBox3 = QtWidgets.QHBoxLayout()
+        hBox4 = QtWidgets.QHBoxLayout()
+        vBox.setSpacing(-20)
+ 
+        #vBox.setMargin(0)
+        # orderNumber
+        self.orderNumber = QtWidgets.QLineEdit("ES345")
+        hBox1.addWidget(QtWidgets.QLabel("orderNumber"))
+        hBox1.addWidget(self.orderNumber)
+    
+        # billCost
+        self.billCost = QtWidgets.QLineEdit("20")
+        hBox2.addWidget(QtWidgets.QLabel("billCost (money only)"))
+        hBox2.addWidget(self.billCost)
+        
+        # quantity
+        self.quantity = QtWidgets.QLineEdit("1")
+        hBox3.addWidget(QtWidgets.QLabel("quantity (integer only)"))
+        hBox3.addWidget(self.quantity)
+        
+        # modelNumber
+        self.modelNumber = QtWidgets.QLineEdit("4255")
+        hBox4.addWidget(QtWidgets.QLabel("modelNumber (integer only)"))
+        hBox4.addWidget(self.modelNumber)
+        
+        vBox.addLayout(hBox1)
+        vBox.addLayout(hBox2)
+        vBox.addLayout(hBox3)
+        vBox.addLayout(hBox4)
+        self.button = QtWidgets.QPushButton('Create new order')
+        self.button.clicked.connect(lambda: self.btn_clk( connection,
+                                                        self.orderNumber.text(), 
+                                                       float(self.billCost.text()), 
+                                                       int(self.quantity.text()), 
+                                                       int(self.modelNumber.text())))
+        vBox.addWidget(self.button)
+        self.setLayout(vBox)
+        
+    def btn_clk(self, connection, orderNumber, billCost, quantity, modelNumber):
+
+        try:  
+            
+            connection = psycopg2.connect(user = "victor6643",
+                                          password = "pass", #Your password in psql
+                                          host = "127.0.0.1",
+                                          port = "5432",
+                                          database = "try4")   #Your db name
+        
+            cursor = connection.cursor()
+            login_sql = """
+                        INSERT INTO orders
+                        VALUES (%s, %d, %d, %d);
+                        """
+   
+            cursor.execute(login_sql, [orderNumber, billCost, quantity, modelNumber]) #Login in ID add password
+            role = cursor.fetchone()[0]    
+            connection.commit()    
+        except (Exception, psycopg2.Error) as error :
+            print ("Error while connecting to PostgreSQL", error)           
+   
+        finally:
+            #closing database connection.
+                if(connection):
+                    pass
+        
 class WindowTwo(QtWidgets.QWidget):
 
     def __init__(self, text):
@@ -42,12 +165,13 @@ class WindowTwo(QtWidgets.QWidget):
         
     def btn_clk(self):
         print("starts")
+        df=""
         try:
             connection = psycopg2.connect(user = "postgres",
-                                          password = "G8h8y8@@", #Your password in psql
+                                          password = "yuzhelim", #Your password in psql
                                           host = "127.0.0.1",
                                           port = "5432",
-                                          database = "test1")   #Your db name
+                                          database = "try1")   #Your db name
             
             query2 = "select * from instructor;"
             
@@ -94,7 +218,7 @@ class AdminWindow(QtWidgets.QWidget):
         
 class Login(QtWidgets.QWidget):
 
-    switch_SaleWindow = QtCore.pyqtSignal()
+    switch_SaleWindow = QtCore.pyqtSignal(psycopg2.extensions.connection)
     switch_AdminWindow = QtCore.pyqtSignal(str)
     def __init__(self):
         QtWidgets.QWidget.__init__(self)
@@ -121,42 +245,32 @@ class Login(QtWidgets.QWidget):
         self.setLayout(vBox)
 
     def login(self, username, pw):
-        success=False
+        role=False
+        connection=''
         try:
-            connection = psycopg2.connect(user = "postgres",
-                                          password = "G8h8y8@@", #Your password in psql
+            connection = psycopg2.connect(user = username,
+                                          password = pw, #Your password in psql
                                           host = "127.0.0.1",
                                           port = "5432",
-                                          database = "test1")   #Your db name
+                                          database = "try4")   #Your db name
         
             cursor = connection.cursor()
-            login_sql = """SELECT id 
-                    from instructor
-                    WHERE name = %s AND dept_name = %s AND
-                    EXISTS (SELECT * FROM instructor WHERE name = %s AND dept_name = %s) 
-                    """
-        
-            cursor.execute(login_sql, (username, pw, username, pw)) #Login in ID add password
-            success = cursor.fetchone()[0]    
+            login_sql = """
+                        with roles(usernam, role) as 
+                        (SELECT r.rolname as username,r1.rolname as "role"
+                        FROM pg_catalog.pg_roles r JOIN pg_catalog.pg_auth_members m
+                        ON (m.member = r.oid)
+                        JOIN pg_roles r1 ON (m.roleid=r1.oid)                                  
+                        WHERE r.rolcanlogin
+                        ORDER BY 1)
+                        select role from roles where usernam = %s;
+                        """
+   
+            cursor.execute(login_sql, [username]) #Login in ID add password
+            role = cursor.fetchone()[0]    
             connection.commit()    
         except (Exception, psycopg2.Error) as error :
-            print ("Error while connecting to PostgreSQL", error)
-        finally:
-            #closing database connection.
-                if(connection):
-                    cursor.close()
-                    connection.close()
-                    print("PostgreSQL connection is closed")
-        if(success):
-            print("User Id and password found in database")
-            a=[success]
-            print(type(success), a)
-            #Switch case for different type of user
-            if '3' in success:
-                self.switch_AdminWindow.emit("Admin")
-            elif '1' in success:
-                self.switch_SaleWindow.emit()    
-        else:
+            #print ("Error while connecting to PostgreSQL", error)           
             print('Error login')
             msg = QtWidgets.QMessageBox()
             msg.setIcon(QtWidgets.QMessageBox.Critical)
@@ -164,6 +278,23 @@ class Login(QtWidgets.QWidget):
             msg.setInformativeText('Please try again.')
             msg.setWindowTitle("Error Login")
             msg.exec_()
+        finally:
+            #closing database connection.
+                if(connection):
+                    pass
+                    #cursor.close()
+                    #connection.close()
+                    #print("PostgreSQL connection is closed")
+        if(role):
+            print("User Id and password found in database")
+            a=[role]
+            #print(type(role), a)
+            #Switch case for different type of user
+            if 'admin' in role:
+                self.switch_AdminWindow.emit("Admin")
+            elif 'sales' in role:
+                self.switch_SaleWindow.emit(connection)    
+
 
 class Controller:
 
@@ -176,13 +307,27 @@ class Controller:
         self.login.switch_AdminWindow.connect(self.show_AdminWindow)
         self.login.show()
 
-    def show_SaleWindow(self):
-        self.sale_Window = saleWindow()
-        self.sale_Window.switch_window.connect(self.show_window_two)
+    def show_SaleWindow(self, connection):
+        self.sale_Window = saleWindow(connection)
+        #self.sale_Window.switch_salesReport.connect(self.show_window_salesReport)
+        self.sale_Window.switch_viewUpdateCus.connect(lambda: self.show_window_viewUpdateCus(connection))
+        self.sale_Window.switch_cr8Order.connect(lambda: self.show_window_cr8Order(connection))
         self.login.close()
         self.sale_Window.show()
         self.sale_Window.setFixedSize(1080,720)
-
+    
+    def show_window_viewUpdateCus(self, connection):
+        self.viewUpdateCus_Window = windowviewUpdateCus(connection)
+        self.sale_Window.close()
+        self.viewUpdateCus_Window.show()
+        self.viewUpdateCus_Window.setFixedSize(1080,720)
+    
+    def show_window_cr8Order(self, connection):
+        self.cr8Order_Window = windowcr8Order(connection)
+        self.sale_Window.close()
+        self.cr8Order_Window.show()
+        self.cr8Order_Window.setFixedSize(1080,720)
+        
     def show_window_two(self, text):
         self.window_two = WindowTwo(text)
         self.admin_Window.close()
